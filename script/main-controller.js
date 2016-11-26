e3c233fd667ce25772060ef767955d0a72b4e135
Ivan Trofimov <richard1997dog@gmail.com>
2016-11-26 13:55:02 +0300

    in work

--- script/main-controller.js
+++ script/main-controller.js
@@ -1,3 +1,109 @@
-app.controller('MainPageController', function MainPageController ($scope) {
+app.controller('MainPageController', function MainPageController ($scope, $mdDialog) {
     $scope.ddd = "456"
+    $scope.subjects = [
+        {
+            group_id: 00,
+            id: 00,
+            title: 'iOS VK',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        },
+        {
+            group_id: 00,
+            id: 01,
+            title: 'Android Cource',
+            flag: 0,
+            color: 'blue',
+            subject_desription: 'Курс ко-ко/ Внимание! Сессия состоится 28 декабря. Не забываем сдавать Ваши проекты вовремя. Да и вообще, пройдите, пожалуйста тест на git.'
+        },
+        {
+            group_id: 00,
+            id: 02,
+            title: 'Mathematics',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        },
+        {
+            group_id: 00,
+            id: 03,
+            title: 'Ochen\' dlinnoe nazvanie',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        },
+        {
+            group_id: 00,
+            id: 04,
+            title: 'Ochen\' dlinnoe nazvanie1',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        },
+        {
+            group_id: 00,
+            id: 05,
+            title: 'Ochen\' dlinnoe nazvanie2',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        },
+        {
+            group_id: 00,
+            id: 06,
+            title: 'Ochen\' dlinnoe nazvanie 3',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        },
+        {
+            group_id: 00,
+            id: 07,
+            title: 'Ochen\' dlinnoe nazvanie 4',
+            flag: 0,
+            color: 'blue',
+            subject_desription: '--'
+        }
+    ];
+    $scope.currentSubject = 00;
+    $scope.currentDate = 00;
+    $scope.subjectTitle = '';
+    $scope.subjectDescription = '';
+    $scope.lessonDescription = '';
+    $scope.lessonTagTag = '';
+    $scope.lastModified = '';
+
+    $scope.clickSubject = function (subject) {
+        $scope.currentSubject = subject.id;
+        $scope.subjectTitle = subject.title;
+        $scope.subjectDescription = subject.subject_desription;
+        $scope.lessonDescription = '';
+        $scope.lessonTagTag = '';
+        $scope.lastModified = '';
+    }
+
+    var alert;
+    $scope.showDialog = showDialog;
+    $scope.items = [1, 2, 3];
+
+
+    function showDialog($event) {
+       var parentEl = angular.element(document.body);
+       $mdDialog.show({
+         parent: parentEl,
+         targetEvent: $event,
+         templateUrl: 'page/demo-dialog.html',
+         locals: {
+           items: $scope.items
+         },
+         controller: DialogController
+      });
+      function DialogController($scope, $mdDialog, items) {
+        $scope.items = items;
+        $scope.closeDialog = function() {
+          $mdDialog.hide();
+        }
+      }
+    }
 });
