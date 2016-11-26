16852b31ef7e542082b247899833b66079905827
Ivan Trofimov <richard1997dog@gmail.com>
2016-11-26 18:31:19 +0300

    in work

--- script/main-controller.js
+++ script/main-controller.js
@@ -1,71 +1,8 @@
 app.controller('MainPageController', function MainPageController ($scope, $mdDialog, $http) {
     $scope.ddd = "456"
-    $scope.subjects = [
-        {
-            group_id: 00,
-            id: 00,
-            title: 'iOS VK',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        },
-        {
-            group_id: 00,
-            id: 01,
-            title: 'Android Cource',
-            flag: 0,
-            color: 'blue',
-            subject_desription: 'Курс ко-ко/ Внимание! Сессия состоится 28 декабря. Не забываем сдавать Ваши проекты вовремя. Да и вообще, пройдите, пожалуйста тест на git.'
-        },
-        {
-            group_id: 00,
-            id: 02,
-            title: 'Mathematics',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        },
-        {
-            group_id: 00,
-            id: 03,
-            title: 'Ochen\' dlinnoe nazvanie',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        },
-        {
-            group_id: 00,
-            id: 04,
-            title: 'Ochen\' dlinnoe nazvanie1',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        },
-        {
-            group_id: 00,
-            id: 05,
-            title: 'Ochen\' dlinnoe nazvanie2',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        },
-        {
-            group_id: 00,
-            id: 06,
-            title: 'Ochen\' dlinnoe nazvanie 3',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        },
-        {
-            group_id: 00,
-            id: 07,
-            title: 'Ochen\' dlinnoe nazvanie 4',
-            flag: 0,
-            color: 'blue',
-            subject_desription: '--'
-        }
-    ];
+    $scope.subjectFlag = false;
+    $scope.datesFlag = false;
+    $scope.subjects = [];
     $scope.dates = []
     $scope.currentSubject = 00;
     $scope.currentDate = 00;
@@ -78,39 +15,32 @@ app.controller('MainPageController', function MainPageController ($scope, $mdDia
     $scope.clickSubject = function (subject) {
         $scope.currentSubject = subject.id;
         $scope.subjectTitle = subject.title;
-        $scope.subjectDescription = subject.subject_desription;
+        $scope.subjectDescription = subject.subject_description;
         $scope.lessonDescription = '';
         $scope.lessonTagTag = '';
         $scope.lastModified = '';
     }
 
     $scope.clickVK = function () {
-        getSubjects()
-        getDates()
+        getSubjects(1)
+        getDates(1)
     }
 
-    $scope.clickPost = function () {
-
-
-        VK.api("wall.post", {"message": "Hello!"}, function (data) {
-            console.log("Post ID:" + data.response.post_id);
-        }, function (data) {
-            console.log("Post: error" + data);
-        });
-    }
-
-    var getSubjects = function () {
-        connectionUrl = 'http://77.244.216.254:8000/api/get/subject/?format=json&group_id=1'
+    var getSubjects = function (id) {
+        connectionUrl = 'http://77.244.216.254:8000/api/get/subject/?format=json&group_id=' + id
         $http.get(connectionUrl).success(function (data) {
-            $scope.subjects = data.objects;
+            $scope.subjects = []
+            for (var i = 0; i < data.objects.length; ++i) {
+                $scope.subjects.push(data.objects[i]);
+            }
             console.log(data);
         }).error(function (data) {
             console.log(data);
         })
     }
 
-    var getDates = function () {
-        connectionUrl = 'http://77.244.216.254:8000/api/get/date/?format=json&subject=1'; 
+    var getDates = function (id) {
+        connectionUrl = 'http://77.244.216.254:8000/api/get/date/?format=json&subject=' + id; 
         $http.get(connectionUrl).success(function (data) {
             $scope.dates = data.objects;
             console.log(data);
